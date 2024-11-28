a4efcd157d1be4b0889b55000924ac75bd2fca60
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2024-11-28 10:06:00 -0330

    added comment

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//this will return the text 'About Page' when user access the web route '/about'
 Route::get('/about', function () {
     return 'About Page';
 });
