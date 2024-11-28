af4a7c90538fef20f533596f510bf1940b882171
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2024-11-28 10:22:53 -0330

    minor update: updated content of all the pages, changed syntax in welcome route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 
@@ -14,6 +14,5 @@ Route::get('/about', function () {
 });
 
 Route::get('/contact', function () {
-    //return ['foo'=>'bar']
     return view('contact');
 });
