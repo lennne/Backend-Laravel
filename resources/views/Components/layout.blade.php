147869228eb67dc84e01eb23211b267bf4f5fa4a
Joseph Acquah <kobia@Josephs-MacBook-Pro.local>
2024-12-17 15:12:22 -0330

    minor update: added component file and dynamically passed in truthy value checking what the current page is and returning a rendered anchor tag

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -26,10 +26,12 @@
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="{{request()->is('/') ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                <a href="/about" class="{{request()->is('about') ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                <a href="/contact" class="{{request()->is('contact') ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-              </div>
+                <!--Utilizing a laravel component called nav-link. It allows us to pass the parameter into a different laravel component.-->
+                <!--In this example we're passing the active value to the component as a php value-->
+                <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                <x-nav-link href="/contact" :active="request()->is('contact')" >Contact</x-nav-link>
+                </div>
             </div>
           </div>
           <div class="hidden md:block">
@@ -79,9 +81,9 @@
         <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
           <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
           <!-- Conditionally render a style attribute -->
-          <a href="/" class="{{request()->is('/') ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
-          <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-          <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
+          <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+          <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+          <x-nav-link href="/contact" :active="request()->is('contact')" >Contact</x-nav-link>
 
         </div>
         <div class="border-t border-gray-700 pb-3 pt-4">
--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,4 +1,9 @@
-<a href="/" class="{{request()->is('/') ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-white" 
-    aria-current="{{request()->is('/') ? 'page':'false'}}">
+@props(['active'=>false])
+@php
+    {{/*it is also possible to have this block of php code in your blade template*/}}
+@endphp
+
+<a class="{{$active ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} block rounded-md  px-3 py-2 text-base font-medium text-white"
+    aria-current="{{$active ? 'page':'false'}}" {{$attributes}}>
 {{$slot}}
 </a>
